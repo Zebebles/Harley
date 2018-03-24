47a5690563dc0af4d77ea9a2c5a2d0684ea486f3
zebebles <zebmuller54@gmail.com>
2018-03-24 11:55:19 +1100

    this might workk

---
+++ commands/misc - math.js
@@ -0,0 +1,30 @@
+const DBF = require('discordjs-bot-framework');
+const math = require('mathjs');
+
+module.exports = class Solve extends DBF.Command{
+    constructor(){
+        super({
+             name: "solve", //is pretty much just another trigger, but can be used filter commands.
+             triggers: ["math"], //any message (excluding msg.client.msg.client.Prefix) that will trigger this command.
+             group: "Misc", //this command will come under this group in the automatic help message.
+             ownerOnly : false, //if this command is to be used by the bot creator only.
+             description: "Let Harley solve a math equation for you.", //this will show in the help message
+             example: ">>solve 9 + 10",             
+			 guildOnly : false, //any command that refers to a guild with the discord.js library will crash if it triggered in a dm channel.  This prevents that.
+			 reqArgs: true
+        });
+    }
+
+    run(params = {"msg": msg, "args": args, "user": user}){ //all the code for your command goes in here.
+        let msg = params.msg; let args = params.args; let user = params.user;
+        if(!args)
+            return msg.channel.send("Usage: `" + msg.guild.prefix +"solve 9+10`.  Use `" + msg.guild.prefix + "commands solve` for more examples.");
+        
+        try{
+            let ans = math.eval(args);
+            msg.channel.send(`I was able to solve \`${args}\`.\nHere's what I got: \`${ans}\``)
+        }catch(err){
+            msg.channel.send("Sorry, I was unable to solve `" + args + "` for you.");
+        }
+     }
+}
