27c964b70e0a5ad62d5cdcfc0275d17d37160573
zebebles <zebmuller54@gmail.com>
2018-03-25 01:26:48 +1100

    badrice init

---
+++ commands/economy - badrice.js
@@ -0,0 +1,73 @@
+const DBF = require('discordjs-bot-framework');
+const Discord = require("discord.js");
+const Promise = require("bluebird");
+
+module.exports = class BlackJack extends DBF.Command{
+    constructor(){
+        super({
+             name: "badrice", //is pretty much just another trigger, but can be used filter commands.
+             triggers: ["br", "3c", "3cups"], //any message (excluding prefix) that will trigger this command.
+             group: "Fun", //this command will come under this group in the automatic help message.
+             ownerOnly : false, //if this command is to be used by the bot creator only.
+             description: "Which bowls of rice are off, and which one is good to eat? Play for fun or bet on which is the good rice.", //this will show in the help message
+             example: ">>badrice\n>>badrice bet\n>>badrice 10\n>>badrice all",             
+             guildOnly : true, //any command that refers to a guild with the discord.js library will crash if it triggered in a dm channel.  This prevents that.
+             reqArgs : true,
+             reqBotPerms : ["ADD_REACTIONS", "MANAGE_MESSAGES"]
+        });
+    }
+
+    run(params = {"msg": msg, "args": args, "user": user}){ //all the code for your command goes in here.
+        let msg = params.msg; let args = params.args; let user = params.user;
+        
+        let amount;
+        if(!args || !args.match(/\d+|half|all/gi))
+            amount = 0;
+        else
+            amount = args.match(/\d+/g) ? parseInt(args.match(/\d+/g)[0]) : (args.match(/all/gi) ? msg.author.rep : Math.floor(msg.author.rep/2));
+        if(amount > msg.author.rep)
+            return msg.channel.send("You only have `" + msg.author.rep + "` rice avaliable to bet.");
+       
+        msg.author.rep > 0 ? 0 :  msg.author.rep = 0;
+        
+
+        let emojis = ["🍛", "🍚", "🍙"];
+        shuffle(emojis);
+        let goodRice = emojis[Math.floor(Math.random() * emojis.length - 2)+1];
+        let embed = new Discord.RichEmbed();
+        embed.setAuthor("Bad Rice | " + msg.author.username, msg.author.displayAvatarURL);
+        embed.setColor(msg.guild.me.displayColor);
+        embed.description = ":japanese_goblin: You have 2 bad rice, 1 good rice. Which one's good?!\n" + emojis.join(" ");
+        
+        msg.channel.send("", {embed}).then(gameMsg => {
+            gameMsg.react(emojis[0]).then(r => gameMsg.react(emojis[1]).then(r => gameMsg.react(emojis[2]).catch(err => console.log(err))).catch(err => console.log(err))).catch(err => console.log(err));
+
+            const filter = (user, reaction) => user.id == msg.author.id && emojis.find(emoji => reaction.emoji.name == emoji);
+            const collector = new Discord.ReactionCollector(gameMsg, filter, {max: 1,time: 20000});
+
+            collector.on("collect", collected => {
+                let chosen = emojis.find(e => e == collected[0].emoji.name);
+                embed.description = goodRice == chosen ? "🤢 You chose the bad rice. " : "😌 You chose the good rice! ";
+                embed.description += amount>0 ? (goodRice == chosen ? "It cost you `" + amount + "` rice." : "You managed to salvage `" + amount*2 + "` rice from the bowl."): "";
+                msg.author.rep += goodRice == chosen ? msg.author.rep += amount*2 : 0;
+                msg.client.syncUser(msg.author);
+                gameMsg.edit("", {embed}).catch(err => console.log(err));
+            }).catch(err => {
+                embed.description = "😞 You waited too long, all the rice went bad!";
+                gameMsg.edit("", {embed}).catch(err => msg.channl.send("", {embed}).catch(err => console.log(err)));
+            });
+        }).catch(err => {
+            msg.author.rep += amount;
+        });
+
+        function shuffle(a) {
+            var j, x, i;
+            for (i = a.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                x = a[i];
+                a[i] = a[j];
+                a[j] = x;
+            }
+        }
+    }
+}
