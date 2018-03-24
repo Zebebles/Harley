e78fd0f161027e641eda76ace4806762008a4200
zebebles <zebmuller54@gmail.com>
2018-03-25 02:42:20 +1100

    better odds

--- commands/fun - ricestack.js
+++ commands/fun - ricestack.js
@@ -35,7 +35,7 @@ module.exports = class BlackJack extends DBF.Command{
         let emojis = ["👆", "🛑"];
         let bowls = ["🍚"];
         let chances = [1, 0.75, 0.5, 0.25, 0.125, 0.0625, 0]
-        let rewards = [1, 1, 1.5, 2, 3];
+        let rewards = [1, 1, 2, 3, 4];
         let embed = new Discord.RichEmbed();
         embed.setAuthor("Rice Stacker | " + msg.author.username, msg.author.displayAvatarURL);
         embed.setColor(msg.guild.me.displayColor);
