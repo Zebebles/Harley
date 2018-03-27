8c5cc5206015ac921273f2e14abac461e9a75f0c
zebebles <zebmuller54@gmail.com>
2018-03-27 14:04:57 +1100

    updated join message

--- classes/client.js
+++ classes/client.js
@@ -37,6 +37,9 @@ class myClient extends DBF.Client {
             guild.defaultTextChannel.send("**Hey! Thanks for adding me! :robot:**\n"
                 +"**•**\tMy default prefix is `" + this.prefix + "`, but you can change it with `"+this.prefix+"prefix new_prefix`.\n"
                 +"**•**\tYou can view my commands with `" + this.prefix + "commands`, or visit my website for a searchable list.\n"
+                +"**•**\tCommands can be disabled either for the whole server or a specific channel with `" + this.prefix + "toggle cmd_name`. May I suggest disabling music in " + guild.defaultChannel + " :wink:\n"
+                +"**•**\tIf you wish to give new members a default role when they join the server, you can do so with `" + this.prefix + "autorole role_name`.\n"
+                +"**•**\tYou can also greet new members, and farewell leaving ones with `" + this.prefix + "greeting message` and `" + this.prefix + "farewell message` respectively.\n"                  
                 +"**•**\tIf you need any help, or have any issues/suggestions, you're always welcome in the support server!\n**<https://discord.gg/Wy5AjGS>**\n"
                 + "**<http://www.harleybot.me/commands>**");
 
