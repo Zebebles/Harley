6ad599a86c6b88811d007e7be9bc65a68a269b87
zebebles <zebmuller54@gmail.com>
2018-03-27 15:31:20 +1100

    xd kappa lol

--- commands/music - queue.js
+++ commands/music - queue.js
@@ -75,7 +75,7 @@ module.exports = class Queue extends DBF.Command{
             let message = "";
             let done = false;
             let ind = 0;
-            for(let i = 1+(5*page); i < 6+(5*page) && i < playlist.queue.length-1; i++){
+            for(let i = 1+(5*page); i < 6+(5*page) && i < playlist.queue.length-2; i++){
                 ind++;
                 message += "\n**" + i + "**\t-\t`" + playlist.queue[i].title + "`";
             }
