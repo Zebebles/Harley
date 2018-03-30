759ece35170fd0e0b70b6492ecc285dc5d19ff45
zebebles <zebmuller54@gmail.com>
2018-03-30 16:09:53 +1100

    wish i remembered sql

--- unlisted/mysql_functions.js
+++ unlisted/mysql_functions.js
@@ -450,7 +450,7 @@ module.exports = function () {
             conn.query("Use Users", (err, res) => {
                 if(err)
                     reject(err);
-                conn.query("SELECT * FROM Users NATURAL JOIN Economy LEFT JOIN Donators ON Users.id = Donators.id;", (err, res) => {
+                conn.query("SELECT * FROM Users NATURAL JOIN Economy FULL JOIN Donators ON Users.id = Donators.id;", (err, res) => {
                     if(err)
                         return reject(err);
                     res.forEach(tuple => {
