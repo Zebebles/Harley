c607affa35d1d32eeb7f0af93c1100005eb2097f
zebebles <zebmuller54@gmail.com>
2018-03-30 16:37:06 +1100

    okay log the tuple

--- unlisted/mysql_functions.js
+++ unlisted/mysql_functions.js
@@ -454,6 +454,7 @@ module.exports = function () {
                     if(err)
                         return reject(err);
                     res.forEach(tuple => {
+                        console.log(tuple);
                         client.fetchUser(tuple.id).then(user => {
                             if(user)
                             {
