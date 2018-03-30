e5ddfd6d12584545862a8786cf9bef3ff9de19db
zebebles <zebmuller54@gmail.com>
2018-03-30 14:01:55 +1100

    that doesnt return a promise

--- classes/playlist.js
+++ classes/playlist.js
@@ -25,7 +25,7 @@ module.exports = class Playlist{
         this.paused = false;
         this.dontRelate = [];
         if(this.message && this.message.collector)
-            this.message.collector.stop().catch(err => err);
+            this.message.collector.stop();
         if(this.message)
             this.message.clearReactions().catch(err => err);
         this.message = null;
