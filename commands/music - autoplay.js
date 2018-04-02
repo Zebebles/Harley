62ad302deba928ace812fdc94fa72635f4741a36
zebebles <zebmuller54@gmail.com>
2018-04-02 15:09:43 +1000

    better autoplay messages

--- commands/music - autoplay.js
+++ commands/music - autoplay.js
@@ -34,9 +34,9 @@ module.exports = class Hello extends DBF.Command{
                 .catch(err => console.log(err));;
 
         if(msg.guild.playlist.auto)
-            msg.channel.send("Related songs will no longer be played when the queue is empty! :)").catch(err => console.log(err));
+            msg.channel.send("Auto play is now **OFF**").catch(err => console.log(err));
         else
-            msg.channel.send("Related songs will now be played when the queue is empty! c:").catch(err => console.log(err));
+            msg.channel.send("Auto play is now **ON**").catch(err => console.log(err));
         
         msg.guild.playlist.auto = !msg.guild.playlist.auto;
     }
