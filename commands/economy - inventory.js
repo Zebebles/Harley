d5c67d83cc3228da78a23ba906b6846eed5f3f6f
zebebles <zebmuller54@gmail.com>
2018-04-05 23:51:45 +1000

    should work propertly

--- commands/economy - inventory.js
+++ commands/economy - inventory.js
@@ -29,14 +29,15 @@ module.exports = class InventoryCmd extends DBF.Command{
             else
                 return item.use(msg);
         }
-
-        let embed = new Discord.RichEmbed()
-        .setAuthor(":school_satchel: Inventory | " + msg.author.username,msg.author.displayAvatarURL)
+        if(!msg.author.items || msg.author.items.length == 0)
+            return msg.channel.send("You don't have any items in your Inventory.\nUse `" + msg.guild.prefix + "store` to buy some items.");
+        let embed = new Discord.RichEmbed() 
+        .setAuthor("🎒 Inventory | " + msg.author.username,msg.author.displayAvatarURL)
         .setColor(msg.guild.me.displayColor)
         .setDescription("Use `inventory use item_name` to use an item.\n");
         msg.author.items.forEach(item => {
             let fullItem = msg.client.store.fetchItem(item.id);
-            embed.description += "\n"+ fullItem.emoji + " **" + fullItem.name + "**\t-\t" + fullItem.description;
+            embed.description += "\n"+ fullItem.emoji + " **" + fullItem.name + "** `("+item.count+")`\t-\t" + fullItem.description;
         });
 
         return msg.channel.send("", {embed});
