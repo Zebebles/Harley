56b22f779390f4fe46d789fae94e990ed3a6b75e
zebebles <zebmuller54@gmail.com>
2018-04-05 23:53:00 +1000

    wooOOOooOhOOooOoo store is working beautifully

--- commands/economy - inventory.js
+++ commands/economy - inventory.js
@@ -32,7 +32,7 @@ module.exports = class InventoryCmd extends DBF.Command{
         if(!msg.author.items || msg.author.items.length == 0)
             return msg.channel.send("You don't have any items in your Inventory.\nUse `" + msg.guild.prefix + "store` to buy some items.");
         let embed = new Discord.RichEmbed() 
-        .setAuthor("🎒 Inventory | " + msg.author.username,msg.author.displayAvatarURL)
+        .setTitle("🎒 Inventory | " + msg.author.username)
         .setColor(msg.guild.me.displayColor)
         .setDescription("Use `inventory use item_name` to use an item.\n");
         msg.author.items.forEach(item => {
