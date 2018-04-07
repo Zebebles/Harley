fb1d512184a912c4bfb1ba079218cf81998dbb23
zebebles <zebmuller54@gmail.com>
2018-04-07 11:24:46 +1000

    init

---
+++ classes/items/donator.js
@@ -0,0 +1,26 @@
+const Item = require("../item.js");
+
+module.exports = class DonatorItem extends Item{
+    constructor () // {id - int, name - string, description - string, price - int, emoji - string, args - string, useImmediately - bool}
+    {
+        super({
+            id: 3,
+            name: "Donator",
+            description: "Full donator status for 1 month.",
+            price: 7500,
+            emoji: ":money_with_wings:",
+            useImmediately: false,
+        });
+    }
+
+    use(msg)
+    {     
+        msg.author.donationTier = 3;
+        msg.author.donationExpires = new Date().getTime() + 2592000000;
+        
+        super.use(msg);
+        msg.channel.send(`${this.emoji} You successfully redeemed your donator status, you now have access to all donator privileges for 30 days!`
+                        + `\nFor a full list of these privileges, see the **Legend** package here **<http://www.harleybot.me/donate/>**`
+                        + `\nIf you haven't already, don't forget to join the Discord server to claim your Donator role. **<https://discord.gg/Wy5AjGS>**`);
+    }
+}
