7728ab03d3cd1b24092142dceba8d529ea78d057
zebebles <zebmuller54@gmail.com>
2018-04-07 11:24:26 +1000

    added functionality to gift items

--- commands/economy - inventory.js
+++ commands/economy - inventory.js
@@ -8,15 +8,16 @@ module.exports = class InventoryCmd extends DBF.Command{
              triggers: ["inv", "backpack", "items"], //any message (excluding prefix) that will trigger this command.
              group: "Economy", //this command will come under this group in the automatic help message.
              ownerOnly : false, //if this command is to be used by the bot creator only.
-             description: "View your inventory, and use any items you have in it.", //this will show in the help message
-             example: ">>inventory\n>>inventory use item_name",             
+             description: "View your inventory and gift or use any items you have in it.", //this will show in the help message
+             example: ">>inventory\n>>inventory use item_name\n>>inventory gift item_name @user",             
              guildOnly : true, //any command that refers to a guild with the discord.js library will crash if it triggered in a dm channel.  This prevents that.
-             reqArgs: true
+             reqArgs: true,
+             reqUser: true
         });
     }
 
     run(params = {"msg": msg, "args": args, "user": user}){ //all the code for your command goes in here.
-        let msg = params.msg, args = params.args;
+        let msg = params.msg, args = params.args, user = params.user;
         
         /*
             Purchase an Item.
@@ -32,6 +33,34 @@ module.exports = class InventoryCmd extends DBF.Command{
             else
             {
                 return item.use(msg);
+            }
+        }
+        else if(args && args.match(/(give)|(gift)|(present)/gi))
+        {
+            let itemID = (args.replace(/(use)|(apply)/gi,"").trim() + " ").split(" ")[0];
+            let item = msg.client.store.fetchItem(itemID);
+            if(!item)
+                return msg.channel.send(`:school_satchel: Sorry, you don't own any \`${itemID}\`'s`);
+            else if( !msg.author.items.find(i => i.id == item.id))
+                return msg.channel.send(`:school_satchel: Sorry, you don't own any \`${item.name}\`'s, but you can buy some with \`${msg.guild.prefix}store buy ${item.name}\``)
+            else if(!user)
+                return msg.channel.send(`:school_satchel: Sorry, I couldn't find the recipient of your gift.`);
+            else
+            {
+                if(msg.author.items.find(i => i.id == item.id).count == 1) //IF THEY ONLY HAVE 1 OF THE ITEM, REMOVE THE ITEM FROM THEIR ARRAY OF ITEMS.
+                    msg.author.items = msg.author.items.filter(i => i.id != item.id);
+                else //IF THEY HAVE MORE THAN 1 OF THE ITEM, DECREASE THEIR ITEM COUNT.
+                    msg.author.items.find(i => i.id == item.id).count--;
+                if(user.items && user.items.find(i => i.id == item.id)) // IF THE USER HAS ONE OR MORE OF THE ITEM ALREADY, INCREASE THEIR COUNT OF THAT ITEM
+                    user.items.find(i => i.id == item.id).count++;
+                else //GIVE THE USER THE ITEM IF THEY DONT HAVE IT ALREADY 
+                    user.items ? user.items.push({id: item.id, count: 1}) : user.items = [{id: item.id, count: 1}];
+                
+                user.itemChanged = item;
+                msg.author.itemChanged = item;
+                
+                msg.client.syncUser(user);
+                msg.client.syncUser(msg.author);
             }
         }
         if(!msg.author.items || msg.author.items.length == 0)
