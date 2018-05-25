62fbe8918a3f3b97322294a9996a683c73d42d82
zebebles <zebmuller54@gmail.com>
2018-05-25 11:38:22 +1000

    try give all the otherCommands the areYou function

--- classes/Socket/Listeners/commands.js
+++ classes/Socket/Listeners/commands.js
@@ -7,8 +7,11 @@ module.exports = class childSocket extends Socket
         const name = "commands";
         
         const fn = (commands) => 
+        {
+            const areYou = client.commands[0].areYou;
             client.otherCommands = commands.filter(cmd => !client.commands.find(c => c.areYou(cmd.name)));
-
+            client.otherCommands.forEach(cmd => cmd.areYou = areYou);
+        }
         super(client, name, fn);
     }
 }
