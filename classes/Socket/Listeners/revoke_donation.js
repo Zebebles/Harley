fb3b16c22e2a4d365c853c81262201ed5de12346
zebebles <zebmuller54@gmail.com>
2018-06-12 13:33:11 +1000

    no need to sync user and stuff

--- classes/Socket/Listeners/revoke_donation.js
+++ classes/Socket/Listeners/revoke_donation.js
@@ -13,14 +13,11 @@ module.exports = class childSocket extends Socket
                     return fn(false);
                 else
                     fn(true);
-                user.donationTier = -1;
-                user.donationExpires = -1;
-
+                    
+                user.send("Your donation has expired");
                 client.guilds.get("317548490928422912").fetchMember(user).then(mem => {
                     mem.removeRoles(["429277014718021644","429277146519830529"]);
                 }).catch(err => console.log("Error fetching member when revoking donation status: " + err));
-                user.send("Your donation has expired");
-                client.syncUser(user);
         }).catch(err => {
             console.log("Error fetching user when revoking donation status: " + err);
             fn(false);
